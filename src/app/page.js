b509e4b54e4c31fc94a5a4b9ed31abdb9d2b0f9f
VandanSonani <vsona2@uic.edu>
2024-08-18 18:53:23 -0500

    Updated Website title name
    Title name update

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,17 @@ import Projects from "./projects/page";
 
 
 
+
+
 export default function Home() {
   return (
+
+
     <main className="grid justify-items-center w-full dark:bg-neutral-900">
+      <head>
+        <title>Vandan's Website</title>
+        <link type="image/x-icon" href="favicon.ico" />
+      </head>
       <div className="mb-2 ">
         <Nav />
         <HomeBody />
